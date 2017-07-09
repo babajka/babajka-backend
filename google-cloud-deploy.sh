90e94dd87d5719cd23203b4917390f006c7c9b5b
uladbohdan <vlbohdan@gmail.com>
2017-07-09 08:47:32 -0700

    Fixing the bash script format

--- google-cloud-deploy.sh
+++ google-cloud-deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # The bash script compiles the server and deploys it to Google Cloud.
 # Require gcloud util to be installed on your machine, check out https://cloud.google.com/sdk/gcloud/
 # Deploying to Google Cloud may require additional permissions.
