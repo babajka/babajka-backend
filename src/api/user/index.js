264bb63414ab5a5bc2433a0d4c1659dff268119b
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2017-07-21 16:11:20 +0300

    add user model and init users method

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
 import * as controller from './controller';
+import User from './model';
 
 const router = Router();
 router.get('/', controller.getAll);
 
+export { User };
 export default router;
---
+++ src/api/user/model.js
@@ -0,0 +1,58 @@
+import mongoose, { Schema } from 'mongoose';
+import { genSalt, hash, compare } from 'bcrypt';
+
+import config from 'config';
+
+const UserSchema = new Schema({
+  id: {
+    type: Number,
+    unique: true,
+    required: true,
+  },
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  passwordHash: String,
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'user', 'creator'],
+    default: 'user',
+  },
+  createdAt: { type: Date, default: new Date() },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  bio: String,
+});
+
+UserSchema.virtual('name')
+  .get(function get() {
+    return `${this.firstName} ${this.lastName}`;
+  });
+
+UserSchema.virtual('password')
+  .get(function get() {
+    return this.passwordHash;
+  });
+
+UserSchema.methods.setPassword = async function set(password) {
+  this.passwordHash = await this.generateHash(password);
+};
+
+UserSchema.methods.generateHash = async (password) => {
+  const salt = await genSalt(config.auth.saltRounds);
+  return hash(password, salt);
+};
+
+UserSchema.methods.authenticate = async function authenticate(password) {
+  return compare(password, this.passwordHash);
+};
+
+const User = mongoose.model('User', UserSchema);
+export default User;
