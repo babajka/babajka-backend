a00302798d8b487d60c6f705e34b33449c457632
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2017-08-11 13:34:10 +0300

    add auth api testing with supertest

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import cors from 'cors';
+import path from 'path';
+
+import api from 'api';
+import config from 'config';
+import getLogger from 'config/logger';
+import auth, { passport, requireAuth } from 'auth';
+
+const publicPath = path.resolve(`${__dirname}/../`, config.publicPath);
+const app = express();
+
+app.set('trust proxy', config.trustProxy);
+app.use(cors());
+app.use(getLogger());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(config.session));
+app.use(express.static(publicPath));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', auth);
+app.use('/api', requireAuth, api);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  console.error(err.message); // eslint-disable-line no-console
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+export default app;
