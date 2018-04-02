f65486be5898cdec22aa7b99d4bf00623c32ac2f
uladbohdan <vlbohdan@gmail.com>
2018-04-02 15:53:24 +0300

    diaries prev/next support

--- src/api/specials/diary/controller.js
+++ src/api/specials/diary/controller.js
@@ -1,5 +1,6 @@
 import { checkDiaryIsFound } from 'utils/validation';
 import { sendJson } from 'utils/api';
+import dateFormat from 'dateformat';
 
 import Diary from './model';
 
@@ -7,6 +8,29 @@ export const getDay = ({ params: { locale, month, day } }, res, next) =>
   Diary.findOne({ locale, colloquialDate: `${month}-${day}`, active: true })
     .select('-_id -__v')
     .then(checkDiaryIsFound)
+    .then(async diary => {
+      const date = new Date(`2018-${month}-${day}`);
+      const prevDate = new Date(date);
+      prevDate.setDate(date.getDate() - 1);
+      const nextDate = new Date(date);
+      nextDate.setDate(date.getDate() + 1);
+
+      const [prevExists, nextExists] = await Promise.all(
+        [prevDate, nextDate].map(curDate =>
+          Diary.findOne({
+            locale,
+            colloquialDate: dateFormat(curDate, 'mm-dd'),
+            active: true,
+          }).then(d => Boolean(d))
+        )
+      );
+
+      return {
+        data: diary,
+        prev: prevExists,
+        next: nextExists,
+      };
+    })
     .then(sendJson(res))
     .catch(next);
 
--- src/api/specials/diary/tests.js
+++ src/api/specials/diary/tests.js
@@ -12,25 +12,62 @@ const request = supertest.agent(app.listen());
 
 describe('Diary API', () => {
   before(async () => {
-    await Diary({
-      author: 'TestAuthor',
-      text: 'SomeDiary',
-      colloquialDate: '02-14',
-      locale: 'be',
-      year: '2018',
-    }).save();
+    await Promise.all([
+      Diary({
+        author: 'Author1',
+        text: 'Diary02-27',
+        colloquialDate: '02-27',
+        locale: 'be',
+      }).save(),
+      Diary({
+        author: 'Author2',
+        text: 'Diary02-28',
+        colloquialDate: '02-28',
+        locale: 'be',
+        year: '2018',
+      }).save(),
+      Diary({
+        author: 'Author3',
+        text: 'Diary03-01',
+        colloquialDate: '03-01',
+        locale: 'be',
+      }).save(),
+    ]);
   });
 
   after(dropData);
 
   describe('# Diary CRUD', () => {
-    it('get an existing diary', () =>
+    it('get an existing diary with prev and next', () =>
       request
-        .get('/api/specials/diary/be/02/14/')
+        .get('/api/specials/diary/be/02/28/')
         .expect(200)
-        .expect(res => {
-          expect(res.body.author).to.equal('TestAuthor');
-          expect(res.body.year).to.equal('2018');
+        .expect(({ body: { data, prev, next } }) => {
+          expect(data.author).to.equal('Author2');
+          expect(data.year).to.equal('2018');
+          expect(data.colloquialDate).to.equal('02-28');
+          expect(prev).to.equal(true);
+          expect(next).to.equal(true);
+        }));
+
+    it('get a diary with next only', () =>
+      request
+        .get('/api/specials/diary/be/02/27')
+        .expect(200)
+        .expect(({ body: { data, prev, next } }) => {
+          expect(data.author).to.equal('Author1');
+          expect(prev).to.equal(false);
+          expect(next).to.equal(true);
+        }));
+
+    it('get a diary with prev only', () =>
+      request
+        .get('/api/specials/diary/be/03/01')
+        .expect(200)
+        .expect(({ body: { data, prev, next } }) => {
+          expect(data.author).to.equal('Author3');
+          expect(prev).to.equal(true);
+          expect(next).to.equal(false);
         }));
 
     it('request unexisting diary', () => request.get('/api/specials/diary/be/02/15').expect(204));
