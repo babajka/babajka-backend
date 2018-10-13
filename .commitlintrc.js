092c6547b30d9142bf10a4a1b0d33cb1d2bd7345
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-10-14 01:21:44 +0300

    fix(tools): add commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        '*',
+        'core',
+        'tools',
+        'utils',
+        'tests',
+        'auth',
+        'user',
+        'article',
+        'collection',
+        'brand',
+        'specials',
+        'config',
+        'db',
+      ],
+    ],
+    'scope-empty': [2, 'never'],
+    'type-enum': [2, 'always', ['fix', 'style', 'feat', 'chore', 'task', 'major', 'merge']],
+  },
+};
