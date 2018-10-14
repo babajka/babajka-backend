0ce254c93a2876259b57d25119ce90f287f4c789
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-10-14 04:02:09 +0300

    fix(tools): fix semantic package.json version update

--- .releaserc.js
+++ .releaserc.js
@@ -15,6 +15,10 @@ module.exports = {
   prepare: [
     '@semantic-release/changelog',
     {
+      path: '@semantic-release/npm',
+      npmPublish: false,
+    },
+    {
       path: '@semantic-release/git',
       assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
       message: '${nextRelease.version} release [skip ci]',
