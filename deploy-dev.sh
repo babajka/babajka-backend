8312c5c1a3a1456e16f7063600fb6b155816f3dc
uladbohdan <vlbohdan@gmail.com>
2018-12-09 18:32:16 +0300

    chore(tools): autostart with pm2

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-rm -rf /home/wir-dev/deployed/backend
-mv /home/wir-dev/deployed/swap-backend /home/wir-dev/deployed/backend
+DEPLOYMENTS_PATH="/home/wir-dev/deployed"
+
+rm -rf "${DEPLOYMENTS_PATH}/backend"
+mv "${DEPLOYMENTS_PATH}/swap-backend/babajka-backend" "${DEPLOYMENTS_PATH}/backend"
+
+cd "${DEPLOYMENTS_PATH}/backend"
+pm2 start pm2-config.json --only=backend-staging
