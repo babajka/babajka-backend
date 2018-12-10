06f6f02da79174ec19999f575aa7ef2ade0f589d
uladbohdan <vlbohdan@gmail.com>
2018-12-10 15:17:51 +0300

    chore(tools): remote machine is ready for a new script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin bash
 set -e
 
 DEPLOYMENTS_PATH="/home/wir-dev/deployed"
@@ -7,4 +7,13 @@ rm -rf "${DEPLOYMENTS_PATH}/backend"
 mv "${DEPLOYMENTS_PATH}/swap-backend/babajka-backend" "${DEPLOYMENTS_PATH}/backend"
 
 cd "${DEPLOYMENTS_PATH}/backend"
-pm2 start pm2-config.json --only=backend-staging
+
+# In order for the command below to work properly make sure that lines
+# case $- in
+#     *i*) ;;
+#       *) return;;
+# esac
+# in ~/.bashrc file on remote machine are commented out.
+#
+# Short explanation is: pm2 is not using interactive ssh.
+pm2 restart pm2-config.json --only=backend-staging
