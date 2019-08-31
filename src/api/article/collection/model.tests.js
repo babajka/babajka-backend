0671adc945938953c78db5bdc193eeca5a5a6cc4
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-09-01 01:39:30 +0300

    fix(db): fix mongoose unique indexes, add tests [#56]

--- src/api/article/collection/model.tests.js
+++ src/api/article/collection/model.tests.js
@@ -13,7 +13,11 @@ describe('ArticleCollection model', () => {
     imageUrl: 'https://collection.jpg',
   };
 
-  before(dropData);
+  before(async function() {
+    this.timeout(5000);
+    await dropData();
+    ArticleCollection.ensureIndexes();
+  });
 
   it('should fail to save collection | no slug', async () => {
     const errorHandler = spy(({ message }) => {
@@ -26,5 +30,21 @@ describe('ArticleCollection model', () => {
       .catch(errorHandler);
 
     expect(errorHandler).to.have.been.called();
+  });
+
+  it('should fail to save collection | dup slug', async () => {
+    const errorHandler = spy(({ message }) => {
+      expect(message).to.not.empty();
+      expect(message).to.includes('duplicate key');
+    });
+
+    const collection = { ...data, slug: 'col-slug' };
+    await ArticleCollection(collection).save();
+    await ArticleCollection(collection)
+      .save()
+      .catch(errorHandler);
+
+    expect(errorHandler).to.have.been.called();
+    await ArticleCollection.find({}).then(collections => expect(collections).to.have.length(1));
   });
 });
--- src/api/user/model.tests.js
+++ src/api/user/model.tests.js
@@ -1,6 +1,6 @@
 import 'db/connect';
 
-import { expect, dropData } from 'utils/testing';
+import { expect, dropData, spy } from 'utils/testing';
 
 import User from './model';
 
@@ -9,7 +9,11 @@ describe('User model', () => {
   const { firstName, email, password } = userData;
   const user = new User(userData);
 
-  before(dropData);
+  before(async function() {
+    this.timeout(5000);
+    await dropData();
+    User.ensureIndexes();
+  });
 
   it('should save user with password', async () => {
     await user.setPassword(password);
@@ -42,6 +46,20 @@ describe('User model', () => {
 
   it('should not authenticate with wrong password', async () =>
     expect(await user.authenticate('not secret')).to.be.false());
+
+  it('should fail to save user | dup email', async () => {
+    const errorHandler = spy(({ message }) => {
+      expect(message).to.not.empty();
+      expect(message).to.includes('duplicate key');
+    });
+
+    await User(userData)
+      .save()
+      .catch(errorHandler);
+
+    expect(errorHandler).to.have.been.called();
+    await User.find({}).then(users => expect(users).to.have.length(1));
+  });
 
   it('should remove user', async () => {
     await User.remove({ email });
