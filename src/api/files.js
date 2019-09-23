6608d492544c23ea5ab70b6490e49a589f69cc52
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-09-23 14:23:43 +0300

    feat(files): add image processing (resize + progressive) [#61]

--- src/api/files.js
+++ src/api/files.js
@@ -1,21 +1,27 @@
 import { Router } from 'express';
 import request from 'request';
+import gm from 'gm';
 
 import config from 'config';
 
 const { host, token } = config.services.fibery;
 
-const filesProxy = async ({ params: { secret } }, res, next) => {
+const filesProxy = async ({ params: { secret }, query: { w = null, h = null } }, res, next) => {
   const url = `https://${host}/api/files/${secret}`;
   const options = {
     url,
     headers: { Authorization: `Token ${token}` },
-    encoding: 'binary',
   };
-  request
-    .get(options)
-    .on('error', next)
-    .pipe(res);
+
+  res.type(`image/png`);
+
+  gm(request.get(options))
+    .resize(w, h)
+    .colors(32)
+    .interlace('Line') // interlaced png or progressive jpeg
+    .stream()
+    .pipe(res)
+    .on('error', next);
 };
 
 const router = Router();
