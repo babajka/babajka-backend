d5ebb268aab2da2331069230752a3bd6224be217
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-09-28 23:14:34 +0300

    task(files): add cli imagesDir option [#61]

--- src/api/files.js
+++ src/api/files.js
@@ -6,11 +6,12 @@ import gm from 'gm';
 
 import config from 'config';
 import { isFileExist } from 'utils/io';
+import { imagesDir } from 'utils/args';
 
 const { host, token } = config.services.fibery;
 
 const filesProxy = async ({ params: { secret }, query: { w = '', f = 'png' } }, res, next) => {
-  const filepath = `images/${secret}_${w}.${f}`;
+  const filepath = `${imagesDir}/${secret}_${w}.${f}`;
   res.type(`image/${f}`);
   const isExist = await isFileExist(filepath);
   if (isExist) {
@@ -29,15 +30,17 @@ const filesProxy = async ({ params: { secret }, query: { w = '', f = 'png' } }, 
 
   request
     .get(options)
-    .pipe(res)
-    .on('error', next);
+    .on('error', next)
+    .pipe(res);
+
+  // TODO: handle 404 (fibery returns `null`)
 
   // asynchronously save file
   gm(request.get(options))
     .resize(w, null)
     .colors(32)
     .interlace('Line') // interlaced png or progressive jpeg
-    .stream()
+    .stream(f)
     .pipe(fs.createWriteStream(filepath));
 };
 
