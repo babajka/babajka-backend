ed808e1a96fc34df07b75f0268642645b27e8733
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2020-04-27 16:46:16 +0300

    fix(article): hack: hardcode soundcloud track ids [#77]

--- src/services/soundcloud.js
+++ src/services/soundcloud.js
@@ -1,12 +1,25 @@
-import fetch from 'node-fetch';
-import qs from 'qs';
+// import fetch from 'node-fetch';
+// import qs from 'qs';
+//
+// import config from 'config';
 
-import config from 'config';
+// FIXME: broken
+// const getResolveUrl = url =>
+//   `http://api.soundcloud.com/resolve.json?${qs.stringify({ url, ...config.services.soundcloud })}`;
+//
+// export const parseSoundcloudUrl = async url => {
+//   const { id } = await fetch(getResolveUrl(url)).then(res => res.json());
+//   return id;
+// };
 
-const getResolveUrl = url =>
-  `http://api.soundcloud.com/resolve.json?${qs.stringify({ url, ...config.services.soundcloud })}`;
+const TRACKS_IDS = {
+  'kashubskaya-mova-lyubo-padporynava': 592430019,
+  'slezskaya-mova-lyubo-padporynava': 647181825,
+  'lemkaskaya-mova-lyubo-padporynava': 612743817,
+  'luzhytskya-movy-lyubo-padporynava': 670133363,
+};
 
 export const parseSoundcloudUrl = async url => {
-  const { id } = await fetch(getResolveUrl(url)).then(res => res.json());
-  return id;
+  const key = url.split('/').pop();
+  return `${TRACKS_IDS[key]}`;
 };
