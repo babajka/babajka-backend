0c3268bde31d994b9773388a4ae6f7f6f4297a74
Ulad Bohdan <vlbohdan@gmail.com>
2020-12-23 01:36:46 +0300

    chore(tools): deploy hacks

--- bin/deploy/install-deps.sh
+++ bin/deploy/install-deps.sh
@@ -3,4 +3,9 @@ set -e
 
 cd "/home/wir-$1/deployed/swap-backend/babajka-backend"
 
+# npm install --build-from-source
+
 npm ci
+
+# https://github.com/kelektiv/node.bcrypt.js/issues/800 ???
+# cd node_modules/bcrypt && node-pre-gyp install --fallback-to-build && cd ../..
