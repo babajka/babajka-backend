6e548780e9033ef3878fde0909d6c1a095785b11
Ulad Bohdan <vlbohdan@gmail.com>
2020-12-25 00:04:08 +0300

    fix(tools): deploy script

--- bin/deploy/install-deps.sh
+++ bin/deploy/install-deps.sh
@@ -3,9 +3,5 @@ set -e
 
 cd "/home/wir-$1/deployed/swap-backend/babajka-backend"
 
-# npm install --build-from-source
-
 npm ci
-
-# https://github.com/kelektiv/node.bcrypt.js/issues/800 ???
-# cd node_modules/bcrypt && node-pre-gyp install --fallback-to-build && cd ../..
+npm run patch-package
