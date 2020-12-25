9e83ea38df5e222743da4371a3b6acf1702b8f46
Ulad Bohdan <vlbohdan@gmail.com>
2020-12-26 01:47:47 +0300

    feat(games): protect metrics endpoint

--- src/api/metrics/counter/index.js
+++ src/api/metrics/counter/index.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
+import { requireAuth, verifyPermission } from 'auth';
+
 import Counter from './model';
 
 import * as controller from './controller';
 
 const router = Router();
 
-router.get('/:key', controller.get);
+router.get('/:key', requireAuth, verifyPermission('canManageArticles'), controller.get);
 
 export { Counter };
 export default router;
