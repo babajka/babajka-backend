97fb1641229a3a71c70331b36114d6f28a9525e7
Ulad Bohdan <vlbohdan@gmail.com>
2021-01-10 21:31:55 +0300

    feat(tests): new youtube url parser

--- src/utils/validation.tests.js
+++ src/utils/validation.tests.js
@@ -34,13 +34,60 @@ describe('Main Page State Validation Tests', () => {
 
 describe('Video: parseYoutubeUrl', () => {
   it('should not accept non-youtube video url', () =>
-    expect(() => parseYoutubeUrl('https://vimeo.com/197700533').to.equal(null)));
+    expect(() => parseYoutubeUrl('https://vimeo.com/197700533').equals(null)));
 
   it('should not accept youtube video url with broken videoId', () =>
-    expect(() => parseYoutubeUrl('https://www.youtube.com/watch?v=LOOOL').to.equal(null)));
+    expect(() => parseYoutubeUrl('https://www.youtube.com/watch?v=LOOOL').equals(null)));
 
-  it('should accept youtube video url', () =>
-    expect(parseYoutubeUrl('https://www.youtube.com/watch?v=OUGvR8UVlSU')).to.be.equal(
-      'OUGvR8UVlSU'
-    ));
+  it('should accept youtube video url [reqular]', () =>
+    expect(parseYoutubeUrl('https://www.youtube.com/watch?v=OUGvR8UVlSU')).equals('OUGvR8UVlSU'));
+
+  it('should accept youtube video url [reqular with space after]', () =>
+    expect(parseYoutubeUrl('https://www.youtube.com/watch?v=OUGvR8UVlSU ')).equals('OUGvR8UVlSU'));
+
+  it('should accept youtube video url [short]', () => {
+    expect(parseYoutubeUrl('https://youtu.be/yrQqzzoeW28')).equals('yrQqzzoeW28');
+  });
+
+  it('should accept youtube video url [short with spaces after]', () => {
+    expect(parseYoutubeUrl('https://youtu.be/yrQqzzoeW28  ')).equals('yrQqzzoeW28');
+  });
+
+  it('should accept youtube video url [embed]', () => {
+    expect(parseYoutubeUrl('http://www.youtube.com/embed/yrQqzzoeW28?rel=0')).equals('yrQqzzoeW28');
+  });
+
+  it('should accept youtube video url [with timing]', () => {
+    expect(parseYoutubeUrl('http://www.youtube.com/watch?v=yrQqzzoeW28#t=0m10s')).equals(
+      'yrQqzzoeW28'
+    );
+  });
+
+  it('should accept youtube video url [with params 1]', () => {
+    expect(
+      parseYoutubeUrl('http://www.youtube.com/v/yrQqzzoeW28?fs=1&amp;hl=en_US&amp;rel=0')
+    ).equals('yrQqzzoeW28');
+  });
+
+  it('should accept youtube video url [with params 2]', () => {
+    expect(
+      parseYoutubeUrl('https://www.youtube.com/watch?v=yrQqzzoeW28&t=76s&ab_channel=WIRBY')
+    ).equals('yrQqzzoeW28');
+  });
+
+  it('should accept youtube video url [with params 3]', () => {
+    expect(
+      parseYoutubeUrl(
+        'https://www.youtube.com/watch?v=btU2yrXImwU&list=PLTyr03fqb0qMIzQnXFCx786QvCG5fyKFC&ab_channel=WIRBY'
+      )
+    ).equals('btU2yrXImwU');
+  });
+
+  it('should not accept youtube video url with broken ID and params', () => {
+    expect(
+      parseYoutubeUrl(
+        'https://www.youtube.com/watch?v=btU2yrXI&list=PLTyr03fqb0qMIzQnXFCx786QvCG5fyKFC&ab_channel=WIRBY'
+      )
+    ).equals(null);
+  });
 });
