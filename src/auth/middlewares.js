0cc694a3c6b320adfcb1ccedcd5682b932d07fab
Ulad Bohdan <vlbohdan@gmail.com>
2021-01-30 15:23:17 +0300

    fix(auth): move jwtAuth to passport file

--- src/auth/middlewares.js
+++ src/auth/middlewares.js
@@ -2,15 +2,13 @@ import HttpStatus from 'http-status-codes';
 
 import { checkPermissions } from 'api/user/model';
 
-import { authenticate } from './passport';
+import { jwtAuth } from './passport';
 
 export const requireAuth = (req, res, next) => {
   if (req.user) {
     // User was already restored from a session.
     return next();
   }
-
-  const jwtAuth = authenticate('jwt', { session: false });
 
   // Attempt to alternatively login with jwt token.
   return jwtAuth(req, res, next)
